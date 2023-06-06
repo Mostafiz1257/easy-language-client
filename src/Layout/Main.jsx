0eca33d7298c76d4f7591ff275ee83dfd4c52770
Mostafiz1257 <mostafiz1257@gmail.com>
2023-06-07 00:08:41 +0600

    checking connection with giuhub

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from '../Pages/Shared/Navbar';
+import Footer from '../Pages/Shared/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <div className=' min-h-[calc(100vh-136px)]'>
+                <Outlet></Outlet>
+            </div>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
